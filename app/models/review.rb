45c66fe439bff6b760ba699769cc418cc383648f
Minja Slavkovic <minja.slavkovic@gmail.com>
2022-02-16 22:15:15 +0000

    adding some css style

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, :rating, presence: true
-  validates :rating, inclusion: 0..5, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5], allow_nil: false }
 end
